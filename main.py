ec69029ae44e34092bcfa2514138d8bad752e749
Egor Ivanov <egor.iwanoow@gmail.com>
2020-05-12 07:10:36 +0300

    New Version
    New move logic with bool expressions. On/Off mode is added. Now can move diagonally. Acceleration was cut but will be added in the future versions.

--- main.py
+++ main.py
@@ -1,44 +1,87 @@
-import pyautogui
-import keyboard
+from pynput import keyboard
+from pynput.mouse import Button, Controller
 
+mouse = Controller()
 
-pyautogui.FAILSAFE = False
+mouseSpeed = 25
 
-isActive = True
+up = False
+down = False
+left = False
+right = False
+isActive = -1
 
-mouseSpeedConst = 15
-mouseSpeed = mouseSpeedConst
-mouseAcceleration = 12
+print("WASD - to move")
+print("e - left click")
+print("f - right click")
+print("q - quit")
+print("Active: " + str(isActive))
+def on_press(key):
+    global up
+    global down
+    global left
+    global right
+    if(isActive == 1):
+        try:
+            if(key.char == 'w'):
+                up = True
+            elif(key.char == 's'):
+                down = True
+            elif(key.char == 'a'):
+                left = True
+            elif(key.char == 'd'):
+                right = True
+            elif(key.char == 'e'):
+                mouse.press(Button.left)
+                mouse.release(Button.left)
+            elif(key.char == 'f'):
+                mouse.press(Button.right)
+                mouse.release(Button.right)
 
-while True:
-    #left
-    if keyboard.is_pressed('a') and isActive:
-        while(keyboard.is_pressed('a')):
-            pyautogui.move(mouseSpeed * -1, 0)
-            mouseSpeed += mouseAcceleration
-        mouseSpeed = mouseSpeedConst
-    #right
-    elif keyboard.is_pressed('d') and isActive:
-        while(keyboard.is_pressed('d')):
-            pyautogui.move(mouseSpeed, 0)
-            mouseSpeed += mouseAcceleration
-        mouseSpeed = mouseSpeedConst
-    #up
-    elif keyboard.is_pressed('w') and isActive:
-        while(keyboard.is_pressed('w')):
-            pyautogui.move(0, mouseSpeed * -1)
-            mouseSpeed += mouseAcceleration
-        mouseSpeed = mouseSpeedConst
-    #down
-    elif keyboard.is_pressed('s') and isActive:
-        while(keyboard.is_pressed('s')):
-            pyautogui.move(0, mouseSpeed)
-            mouseSpeed += mouseAcceleration
-        mouseSpeed = mouseSpeedConst
-    #left click
-    elif keyboard.is_pressed('e') and isActive:
-        pyautogui.click()
-    elif keyboard.is_pressed('`'):
-        isActive = True
-    elif keyboard.is_pressed('\\'):
-        isActive = False
+            if(up):
+                mouse.move(0, -mouseSpeed)
+            if(down):
+                mouse.move(0, mouseSpeed)
+            if(left):
+                mouse.move(-mouseSpeed, 0)
+            if(right):
+                mouse.move(mouseSpeed, 0)
+
+        except AttributeError:
+            pass
+
+def on_release(key):
+    global up
+    global down
+    global left
+    global right
+    global isActive
+    try:
+        if(key.char == 'w'):
+            up = False
+        elif(key.char == 's'):
+            down = False
+        elif(key.char == 'a'):
+            left = False
+        elif(key.char == 'd'):
+            right = False
+        elif(key.char == '`'):
+            isActive = isActive*-1
+            print("Active: " + str(isActive))
+
+        if key.char == 'q':
+            # Stop listener
+            return False
+    except AttributeError:
+        pass
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
--- readme.txt
+++ readme.txt
@@ -2,13 +2,14 @@ Description:
 	Program for moving mouse with keyboard. 
 Usage:
 	Hot Keys: 
-		` - on 
-		\ - off 
+		` - on\off
+		q - off 
 		w - up 
 		d - down 
 		a - left 
 		d - right
 		e - right click
+		f - left click
 	Install: 
 		1) clone or just download repo
 		2) $python -m venv venv
